4778bed1f2a576503aedc2466fa14e809ec3c56c
Brandon Yee <brandonyee@Brandons-MacBook-Air.local>
2024-11-17 22:05:33 -0500

    Added main.c and skeleton func to execute all linkedList helper functions

---
+++ src/linkListHelper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <linkListHelper.h>
+
+
+
+void testLinkedList()
+{
+    printf("This function was called");
+}
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <linkListHelper.h>
+
+int main()
+{
+    testLinkedList();
+}
